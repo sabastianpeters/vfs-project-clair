26b24cfb882f8c5773bcb6c5bbdf5e80c398c0a5
Sabastian <sabastianp@hotmail.ca>
2020-06-23 00:14:27 -0700

    fixed: logos are now locally loaded

--- project_clair/lib/screens/camera_screen.dart
+++ project_clair/lib/screens/camera_screen.dart
@@ -15,10 +15,9 @@ class CameraScreen extends StatelessWidget {
         
         return Scaffold(
             appBar: AppBar(
-                title: Image.network(
-                    "https://framerusercontent.com/projects/piSGerPqUbtGA1EE399C/assets/4096/CQ7n2opjGvmc8NP3IaQWhCeXObM.jpg?Expires=1592957114&Key-Pair-Id=APKAIDY47RVYVKGA6NJQ&Signature=KoNOnCSnkGBOGfqx8Lcf4rUZD3Gi8m~fgI9yIU7SDoFbsQ3h6ktdk51dR9Z-jd3Jy2EnMf2pCzwZHSTM3sz6z1KWsw8WVvRlx73DD~2gBDKv5EdOvtjZOK7q3qt7VgjKHN1EoXlMuEqyCrdPEpEMFbiNOVV47DoAyAHjEl6uggGWTr9loOwqbDsxqa7B7I5zBnF~12w38u0ElMQyAhKeHu0mpjziapEKiBGW7TnJ2MFB6uHXOMiHS9pd8KNQwta2LP0mbX5NC0XxQY8ZiC4OUWVGnwvurtz4MZpCTIyeNGuqYu9JzFRn~k7hCXGahTps0IhTPUh37UbBQ-wDsXOMWw__",
-                    width: 177 * 1.2,
-                    height: 107 * 1.2,
+                title: Image.asset(
+                    "assets/logo_icon.png",
+                    height: 32,
                 ),
                 centerTitle: true,
             ),
--- project_clair/lib/screens/landing_screen.dart
+++ project_clair/lib/screens/landing_screen.dart
@@ -20,15 +20,15 @@ class LandingScreen extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
                     
-                        Image.network(
-                            "https://framerusercontent.com/projects/piSGerPqUbtGA1EE399C/assets/4096/CQ7n2opjGvmc8NP3IaQWhCeXObM.jpg?Expires=1592957114&Key-Pair-Id=APKAIDY47RVYVKGA6NJQ&Signature=KoNOnCSnkGBOGfqx8Lcf4rUZD3Gi8m~fgI9yIU7SDoFbsQ3h6ktdk51dR9Z-jd3Jy2EnMf2pCzwZHSTM3sz6z1KWsw8WVvRlx73DD~2gBDKv5EdOvtjZOK7q3qt7VgjKHN1EoXlMuEqyCrdPEpEMFbiNOVV47DoAyAHjEl6uggGWTr9loOwqbDsxqa7B7I5zBnF~12w38u0ElMQyAhKeHu0mpjziapEKiBGW7TnJ2MFB6uHXOMiHS9pd8KNQwta2LP0mbX5NC0XxQY8ZiC4OUWVGnwvurtz4MZpCTIyeNGuqYu9JzFRn~k7hCXGahTps0IhTPUh37UbBQ-wDsXOMWw__",
-                            width: 177 * 1.2,
-                            height: 107 * 1.2,
+                        Image.asset(
+                            "assets/logo.png",
+                            width: 177,
+                            height: 107,
                         ),
 
                         Container(
                             child: Text(
-                                "Clair, our transcribe assistant is here to help you",
+                                "Take down notes with our transcribe assistant, Clair.",
                                 textScaleFactor: 1.8,
                             ),
                         ),
